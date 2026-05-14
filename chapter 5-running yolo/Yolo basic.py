97e20145cc3b26e8d6ea93a52cc70872347f169b
Chamanjangid <chamanjangir.work@gmail.com>
2026-05-14 14:53:06 +0530

    Delete chapter 5-running yolo directory

--- chapter 5-running yolo/Yolo basic.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-model = YOLO('../yolo-weight/yolov8n.pt')
-results = model("image/1.png", show=True)
-img = results[0].plot()
-cv2.imshow("YOLO Result", img)
-cv2.waitKey(0)
